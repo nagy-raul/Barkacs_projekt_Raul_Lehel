07a28cde396b00f548926048de6c9f1d962e2016
nagy-raul <nagy.raul-2020@keri.mako.hu>
2024-01-14 21:37:00 +0100

    01.14 changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-function topnapResponsive() {
+function topnavResponsive() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
