b459a2c4a730a2e5b17896e9c213d875ea9ada7c
nagy-raul <nagy.raul-2020@keri.mako.hu>
2024-03-04 17:23:15 +0100

    blog időrendi sorrend

--- js/app.js
+++ js/app.js
@@ -5,4 +5,15 @@ function topnavResponsive() {
     } else {
       x.className = "topnav";
     }
-  }
+}
+
+let op1 = document.querySelector("#op1");
+let op2 = document.querySelector("#op2");
+let op3 = document.querySelector("#op3");
+let op4 = document.querySelector("#op4");
+
+let blog1 = document.querySelector("#painting");
+
+if(op2.selected == true){
+  blog1.hidden == true;
+}
