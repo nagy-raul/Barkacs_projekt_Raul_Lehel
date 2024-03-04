7e24528cbe06c27067547cbfc6939435be05fed3
nagy-raul <nagy.raul-2020@keri.mako.hu>
2024-03-04 18:30:41 +0100

    blog évszerinti sorrend

--- js/app.js
+++ js/app.js
@@ -13,7 +13,34 @@ let op3 = document.querySelector("#op3");
 let op4 = document.querySelector("#op4");
 
 let blog1 = document.querySelector("#painting");
+let blog2 = document.querySelector("#henger");
+let blog3 = document.querySelector("#brush");
+let blog4 = document.querySelector("#yinyang");
 
-if(op2.selected == true){
-  blog1.hidden == true;
+
+
+
+function onSelect(el){
+  const option = el.value;
+  if (option === 'years'){
+    blog1.hidden = false;
+    blog2.hidden = false;
+    blog3.hidden = false;
+    blog4.hidden = false;
+  } else if (option === '2024'){
+    blog1.hidden = true;
+    blog2.hidden = true;
+    blog3.hidden = true;
+    blog4.hidden = false;
+  } else if (option === '2023'){
+    blog1.hidden = true;
+    blog2.hidden = false;
+    blog3.hidden = false;
+    blog4.hidden = true;
+  } else if (option === '2022'){
+    blog1.hidden = false;
+    blog2.hidden = true;
+    blog3.hidden = true;
+    blog4.hidden = true;
+  }
 }
